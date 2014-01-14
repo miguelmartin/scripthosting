9e7eeca18b3da93d27a33e4b6283a28d5e541fcd
root <root@debian.usuario>
2014-01-14 17:39:49 +0100

    Terminar DNS y apache

--- alta.py
+++ alta.py
@@ -1,5 +1,4 @@
 # ­*­ coding: utf­8 ­*­
-
 import os
 import sys
 
@@ -7,6 +6,7 @@ user_name = sys.argv[1]
 
 domain_name = sys.argv[2]
 
+#Añadimos al fichero /etc/bind/named.conf.local las zonas nuevas
 
 linea1 = '\nzone ' +'"' +  domain_name +'"'  +'{\ntype master;\nfile "db.'+ domain_name +'"' +' ;\n}; '
 linea2 = '\nzone "45.168.192.in-addr-arpa" {\ntype master;\nfile "db.45.168.192";\n};\n'
@@ -15,5 +15,39 @@ fichero.write(linea1)
 fichero.write(linea2)
 fichero.close 
 
+#Creamos los ficheros de las zonas nuevas
 
+plantillazona = open("plantillazona","r")
+lineas = plantillazona.readlines() 
+plantillazona.close
+ficherozona = open("/var/cache/bind/db."+domain_name+"","w")
+for linea in lineas:
+	linea = linea.replace('domain_name',domain_name)
+	ficherozona.write(linea)
+ficherozona.close()
+	
+plantillainversa = open("plantillainversa","r")
+lineas2 = plantillainversa.readlines()
+plantillainversa.close
+ficheroinversa = open("/var/cache/bind/db.45.168.192","w")
+for linea2 in lineas2:
+        linea2 = linea2.replace('domain_name',domain_name)
+        ficheroinversa.write(linea2)
+ficheroinversa.close()
 
+os.system("service bind9 restart")
+
+#Creamos el nuevo virtualhost y directorio web
+
+os.mkdir("/var/"+domain_name+"")
+
+plantillahost = open("plantillahost","r")
+lineas3 = plantillahost.readlines()
+plantillahost.close
+ficherohost = open("/etc/apache2/sites-available/"+domain_name+"","w")
+for linea3 in lineas3:
+        linea3 = linea3.replace('domain_name',domain_name)
+        ficherohost.write(linea3)
+ficherohost.close()
+
+os.system("service apache2 restart")
