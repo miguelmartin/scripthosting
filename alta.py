9a1f17268febf0d682850d3bd67bca5720e208f5
root <root@hosting-miguemartin.novalocal>
2014-01-14 10:40:09 +0100

    añadir zonas

--- alta.py
+++ alta.py
@@ -6,3 +6,14 @@ import sys
 user_name = sys.argv[1]
 
 domain_name = sys.argv[2]
+
+
+linea1 = '\nzone ' +'"' +  domain_name +'"'  +'{\ntype master;\nfile "db.'+ domain_name +'"' +' ;\n}; '
+linea2 = '\nzone "45.168.192.in-addr-arpa" {\ntype master;\nfile "db.45.168.192";\n};\n'
+fichero = open("/etc/bind/named.conf.local","a")
+fichero.write(linea1) 
+fichero.write(linea2)
+fichero.close 
+
+
+
