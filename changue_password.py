1d67fe06b59f10183815cc81ce3ed6e16c85ffb8
root <root@hosting-miguemartin.novalocal>
2014-02-14 09:24:58 +0100

    FIcheor para cambiar passwd

---
+++ changue_password.py
@@ -0,0 +1,25 @@
+# ­*­ coding: utf­8 ­*­
+import sys
+import MySQLdb
+
+#Variables que puede recibir el programa
+opcion=(sys.argv[1])
+user_name=(sys.argv[2])
+passwdnew=(sys.argv[3])
+
+if opcion == "-mysql":
+        host = MySQLdb.connect(host="localhost", user="root", passwd="usuario", db="proftpd")
+        cursor=host.cursor()
+        queryuser="select userid from ftpuser where userid='"+user_name+"';"
+        cursor.execute(queryuser)
+        resultado = cursor.fetchone()
+        if resultado[0] == user_name:
+                changuepasswd="SET PASSWORD FOR "+user_name+"@localhost = PASSWORD('"+passwdnew+"');"
+                cursor.execute(changuepasswd)
+                host.commit()
+                print "Se ha actualizado la contraseña del usuario correctamente"
+        else:
+                print "El usuario introducido no existe"
+               	exit()
+
+elif opcion == "-ftp":
